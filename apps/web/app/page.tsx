9de6db7f9e2a1cbac9ec5e5867819029b84a6f20
Sonal Singh <sonal1201kumar@gmail.com>
2025-07-01 22:05:49 +0530

    ..

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
-
 import { client } from "@repo/db/client";
 
 export default async function Home() {
   const user = await client.user.findFirst();
-  return <div>
 
-    {user?.username}
-    {user?.password}
-
-  </div>;
+  return (
+    <div>
+      <h1>CHANDAN PASIA BEJ RE LAVDE 160</h1>
+      <div>
+        <p>Username: {String(user?.username)}</p>
+      </div>
+    </div>
+  );
 }
